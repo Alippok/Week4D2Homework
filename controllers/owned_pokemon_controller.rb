ed6ad508957747c2cadf0b5c2d14193f6be53515
A_Lippok <alippok@hotmail.co.uk>
2016-03-09 08:29:50 +0000

    Adds owned_pokemon controller and 3 rrestful routes and actions to: get a form for a new owned pokemon, add a pokemon to a trainer and show pokemon who are owned

---
+++ controllers/owned_pokemon_controller.rb
@@ -0,0 +1,28 @@
+require_relative('../models/pokemon.rb')
+require_relative('../models/trainer.rb')
+require_relative('../models/owned_pokemon.rb')
+require('pry-byebug')
+
+
+get '/owned_pokemon/new' do
+  @pokemons = Pokemon.all
+  @trainers = Trainer.all
+
+  erb(:"owned_pokemon/new")
+end
+
+
+post '/owned_pokemon' do
+  @owned_pokemon = OwnedPokemon.new( params )
+
+  @owned_pokemon.save()
+  redirect to '/'
+end
+
+get '/owned_pokemon' do 
+  @owned_pokemons = OwnedPokemon.join1
+  # pry.byebug
+  erb(:"owned_pokemon/index")
+end
+
+
